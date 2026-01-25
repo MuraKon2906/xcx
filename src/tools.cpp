1d17f585358eb239a0bfdd9d56b8b91812db4f7d
Murakon2906 <mcray2906writes@gmail.com>
2026-01-25 15:31:30 +0530

    cmake now builds binaries automatically

--- src/tools.cpp
+++ src/tools.cpp
@@ -4,8 +4,14 @@
 #include <toml++/toml.h>
 #include <xcx/tools.hpp>
 
-namespace fs = std::filesystem;
-PrjBuild::PrjBuild() { _cmake_setup(); }
+PrjBuild::PrjBuild() {
+  if (!_is_build()) {
+    std::cout << "XCX : Build doesn't exist. Aborting.\n";
+    return;
+  }
+  _cmake_setup();
+  _create_binaries();
+}
 
 bool PrjBuild::_is_build() {
   if (std::filesystem::is_directory("build")) {
@@ -17,13 +23,11 @@ bool PrjBuild::_is_build() {
 }
 
 void PrjBuild::_cmake_setup() {
-  if (!_is_build()) {
-    std::cout << "XCX : Build doesn't exist. Aborting.\n";
-    return;
-  }
+  std::cout << "\n Creating the necessary Build System files : \n";
+  std::system("cmake -S . -B build");
+}
+void PrjBuild::_create_binaries() {
 
-  fs::path build_dir = fs::current_path() / "build";
-  fs::path cmd = "cmake -S . -B " + build_dir.string();
-
-  std::system(cmd.string().c_str());
+  std::cout << "\n Building Project Binaries \n";
+  std::system("cd build && cmake --build .");
 }
