958b87de2ba0fe47bd98de84423a4f0e0c92a875
Murakon2906 <mcray2906writes@gmail.com>
2026-01-25 14:05:49 +0530

    dummy commit

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,18 +1,9 @@
 #include <filesystem>
 #include <iostream>
+#include <toml++/toml.h>
 #include <xcx/tools.hpp>
 namespace fs = std::filesystem;
 
-void _check_build(fs::path prj_path) {
-  fs::path build_path = prj_path / "build";
-  std::cout << "\b checking contents of build folder : ";
-  try {
-    if (fs::is_empty(build_path)) {
-      std::cout << "Contents of Build folder are empty : ";
-    } else {
-    }
-  } catch (...) {
-    std::cout << "Error has occured while determining file contents in the "
-                 "build folder \n";
-  }
-}
+PrjBuild::PrjBuild() {}
+
+bool PrjBuild::_is_build() {}
